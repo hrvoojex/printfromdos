44d5d7167bc489aa4606d158084bc50eba5ec430
hrvooje <hrvooje@gmail.com>
2017-06-20 10:42:16 +0200

    Added remove file function for pdf, pcl and txt file. Added prnting from pdf to default printer

--- print_rawpcl.py
+++ print_rawpcl.py
@@ -1,32 +1,44 @@
 #!/usr/bin/env python3
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 """
 Autor: Hrvoje T
 Last edit: June 2017
-pip instal reportlab
-pip install pypiwin32 --> win32api, za python 2 ide python -m pip install 
-io module, io.open
-ako postoje argumenti komandne linije,
-prvi argument je ime datoteke za print
+
+'pip install pypiwin32 --> for installing win32api'
+In python 2: 'python -m pip install pypiwin32'
+io module for io.open in Python2, the same as 'open' in Python3
+First command line argument is for file name which we want to print:
+'python print_rawpcl.py my_pcl_text_file.txt'
 """
 
-import os
-import sys
-import io
-import win32print
-import win32api
-import subprocess
+import os, sys, io, win32print, win32api, subprocess
 
 
+def remove_silently(file1):
+    """Removing silently files from last time if there are any left"""
+    try:
+        os.remove(file1)
+    except OSError:
+        pass
+
+# Asign your printers
 first_default_printer = win32print.GetDefaultPrinter()
 tmp_printer = "local_pcl"
+# This 'print.pcl' is an output from virtual 'local_pcl' printer
+my_pcl_file = "print.pcl"
+my_output_pdf = "print.pdf"
 
+remove_silently(my_output_pdf)
+remove_silently(my_pcl_file)
+
+# If there is command line argument, the first one is our file_to_print
 if len(sys.argv) > 1:
     file_to_print = sys.argv[1]
 else:
     file_to_print = "RACUN.TXT"
 
+# HP LJ P2035 supports PCL, so no need to choose different printer
 if "2035" in first_default_printer:
     # do nothing if a printer is supporting PCL
     pass
@@ -34,14 +46,16 @@ else:
     # if the printer is not supporting PCL and can't print RAW data
     # change default printer to local_pcl 
     win32print.SetDefaultPrinter(tmp_printer)
-    
+
+# Printing RAW data to the virtual 'local_pcl' printer or to the 'HP LJ P2035'
 try:
-    # rb je za 'read, bytes', string je byte ne enkodiran tekst
-    with io.open(file_to_print,'rb') as f:
+    # rb --> 'read, bytes', string is 'bytes' type, not unicode (Python3)
+    with io.open(file_to_print, 'rb') as f:
         raw_data = f.read()
         hPrinter = win32print.OpenPrinter(win32print.GetDefaultPrinter())
         try:
-            hJob = win32print.StartDocPrinter(hPrinter, 1,("print_rawpcl.py data", None, "RAW"))
+            hJob = win32print.StartDocPrinter(hPrinter, 1, (
+                    "print_rawpcl.py data", None, "RAW"))
             try:
                 win32print.StartPagePrinter(hPrinter)
                 win32print.WritePrinter(hPrinter, raw_data)
@@ -50,23 +64,26 @@ try:
                 win32print.EndDocPrinter(hPrinter)
         finally:
             win32print.ClosePrinter(hPrinter)
-    # uklanja datoteke nakon printanja istih
-    #os.remove(file_to_print)
 except OSError as e:
-    print("Pojavila se greška: {}".format(e))
+    print("Failed: {}".format(e))
 
-# Convert pcl file to pdf if the default printer is local_pcl
+converter_app = "WinPCLtoPDF.exe"
+# Convert a pcl file to pdf with WinPCLtoPDF.exe
+# if the default printer is local_pcl
 if win32print.GetDefaultPrinter() == "local_pcl":
-    # convert PCL file to PDF file with WinPCLtoPDF.exe
-    #subprocess.Popen(['"C:\tmp\WinPCLtoPDF.exe C:\tmp\print.pcl C:\tmp\output.pdf"'])
-    subprocess.call(['C:/Python34/WinPCLtoPDF.exe', 'print.pcl'])
+    subprocess.call([converter_app, my_pcl_file])
 
 # return default printer to the printer before running this script
 win32print.SetDefaultPrinter(first_default_printer)
-
+gsprint_app = "C:\\Program Files\\Ghostgum\\gsview\\gsprint.exe"
 # Finally, print that print.pdf to your first default printer silently
-file_path = "C:\\Python34\\print.pdf"
-p = subprocess.Popen(["C:\\Program Files\\Ghostgum\\gsview\\gsprint.exe", file_path],
+p = subprocess.Popen(
+        [gsprint_app, my_output_pdf],
         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-stdout, stderr = p.communicate() # waits for the gs process to end
-os.remove(file_path) # now the file can be removed
+# Waits for the gs process to end
+stdout, stderr = p.communicate()
+# Remove print.pcl and print.pdf file
+remove_silently(my_output_pdf)
+remove_silently(my_pcl_file)
+remove_silently(file_to_print)
+
