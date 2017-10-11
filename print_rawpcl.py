0394c23f51ec237b49755447c0d87a41aec7f8dd
hrvooje <hrvooje@gmail.com>
2017-10-11 14:51:51 +0200

    Testing shell execute command

--- print_rawpcl.py
+++ print_rawpcl.py
@@ -14,49 +14,58 @@ First command line argument is for file name which we want to print:
 'python print_rawpcl.py myPCLfile.txt'
 """
 
-import os
-import sys
-import io
-import time
+# import os
+# import sys
+# import io
+# import time
+# import win32print
+#
+#
+# # If there is command line argument, the first one is our file_to_print
+# if len(sys.argv) > 1:
+#     file_to_print = sys.argv[1]
+# else:
+#     file_to_print = "RACUN.TXT"
+#
+# # Printing RAW data to the default printer
+# try:
+#     # rb --> 'read, bytes', string is 'bytes' type, not unicode (Python3)
+#     with io.open(file_to_print, 'rb') as f:
+#         raw_data = f.read()
+#         hPrinter = win32print.OpenPrinter(win32print.GetDefaultPrinter())
+#         try:
+#             hJob = win32print.StartDocPrinter(hPrinter, 1, (
+#                     "print_rawpcl.py data", None, "RAW"))
+#             try:
+#                 win32print.StartPagePrinter(hPrinter)
+#                 win32print.WritePrinter(hPrinter, test_str)
+#                 win32print.EndPagePrinter(hPrinter)
+#             finally:
+#                 win32print.EndDocPrinter(hPrinter)
+#         finally:
+#             win32print.ClosePrinter(hPrinter)
+# except OSError as e:
+#     print("Failed: {}".format(e))
+#
+# # Message at the end of execution
+# print("Script finished successfully. Everything OK!")
+# time.sleep(2)  # Wait 2 seconds for reader to read
+
+import tempfile
+import win32api
 import win32print
 
-
-def remove_silently(file1):
-    """Removing silently files from last time if there are any left"""
-    try:
-        os.remove(file1)
-    except OSError:
-        pass
-
-# Asign your printers
-first_default_printer = win32print.GetDefaultPrinter()
-
-# If there is command line argument, the first one is our file_to_print
-if len(sys.argv) > 1:
-    file_to_print = sys.argv[1]
-else:
-    file_to_print = "RACUN.TXT"
- 
-# Printing RAW data to the default printer
-try:
-    # rb --> 'read, bytes', string is 'bytes' type, not unicode (Python3)
-    with io.open(file_to_print, 'r') as f:
-        raw_data = f.read()
-        hPrinter = win32print.OpenPrinter(win32print.GetDefaultPrinter())
-        try:
-            hJob = win32print.StartDocPrinter(hPrinter, 1, (
-                    "print_rawpcl.py data", None, "RAW"))
-            try:
-                win32print.StartPagePrinter(hPrinter)
-                win32print.WritePrinter(hPrinter, raw_data)
-                win32print.EndPagePrinter(hPrinter)
-            finally:
-                win32print.EndDocPrinter(hPrinter)
-        finally:
-            win32print.ClosePrinter(hPrinter)
-except OSError as e:
-    print("Failed: {}".format(e))
-
-# Message at the end of execution
-print("Script finished successfully. Everything OK!")
-time.sleep(2)  # Wait 2 seconds for reader to read
+filename = tempfile.mktemp (".txt")
+open (filename, "w").write ("This is a test")
+win32api.ShellExecute (
+  0,
+  "print",
+  filename,
+  #
+  # If this is None, the default printer will
+  # be used anyway.
+  #
+  '/d:"%s"' % win32print.GetDefaultPrinter (),
+  ".",
+  0
+)
